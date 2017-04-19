27ef6cc094707b18984c3eae6c3d2cd437ee1902
Gurkeerat Singh <gurkeerat.singh.96@gmail.com>
2017-04-19 21:11:42 +0200

    JsonService falla al obtener la instancia

--- src/main/java/JSonService/JSonService.java
+++ src/main/java/JSonService/JSonService.java
@@ -1,7 +1,46 @@
 package JSonService;
+import Controller.EtackemonManager;
+import Model.Users;
+
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Gurkeerat Singh on 19/04/2017.
  */
+
+@Path("/json")
+@Singleton
+
 public class JSonService {
+
+        protected EtackemonManager etackemonManager;
+        public JSonService() {
+
+        }
+
+        @GET
+        @Path("/tenertodos")
+        @Produces(MediaType.APPLICATION_JSON)
+        public List<Users> getList() {
+            List<Users> a = new ArrayList<Users>();
+            a= etackemonManager.getInstance().listUsers();//Falla al obtener la instancia
+            return a;
+        }
+        @POST
+        @Path("/addUser")
+        @Produces(MediaType.APPLICATION_JSON)
+        public void setUser(Users user){
+            etackemonManager.getInstance().addUser(user);//Falla al obtener la instancia
+        }
+        @POST
+        @Path("/updateUser")
+        @Produces(MediaType.APPLICATION_JSON)
+        public void modificarUser(String name, String newName, String mail){
+            etackemonManager.getInstance().updateUser(name,newName,mail);//Falla al obtener la instancia
+        }
+
 }
